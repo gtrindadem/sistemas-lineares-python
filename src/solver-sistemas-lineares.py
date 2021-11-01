a8a903f01012c6b220089aba35878a1eda9281ac
Guilherme Trindade <guilherme.01.gt@gmail.com>
2021-11-01 20:46:54 -0300

    Adiciona dialog para escolha do arquivo

--- src/solver-sistemas-lineares.py
+++ src/solver-sistemas-lineares.py
@@ -1,6 +1,12 @@
 import os
 from utils.leitor_csv import csv_para_list
 from ortools.linear_solver import pywraplp
+import tkinter as tk
+from tkinter import filedialog
+
+# Limpa root do tkinter para não abrir nenhum dialog adicional
+root = tk.Tk()
+root.withdraw()
 
 def set_coefficient(constraint, variavel, coefficient):
     constraint.SetCoefficient(variavel, float(coefficient))
@@ -14,7 +20,7 @@ def cria_constraint(solver, array_de_variaveis, array_de_coeficientes):
         set_coefficient(ct, var, array_de_coeficientes[i])
 
 def printar_sistema(solver, variaveis):
-    print()
+    print('\nSISTEMA LINEAR:')
 
     constraints = solver.constraints()
 
@@ -29,9 +35,6 @@ def printar_sistema(solver, variaveis):
                 print(f'{constraint.GetCoefficient(var)}', end=' ')
             print(f'* {var}', end=' ')
         print(f'= {constraint.lb()}')
-    
-    print()
-
 
 def solve_sistema_linear(input: list):
 
@@ -50,13 +53,11 @@ def solve_sistema_linear(input: list):
     status = solver.Solve()
 
     if (status == pywraplp.Solver.OPTIMAL):
+        print('\nRESULTADO:')
         for i, x in enumerate(variaveis):
             print(f'x{i} = {x.solution_value()}')
     else:
         print('Não existe uma solução ótima para este sistema!')
-
-
-# Main
 
 # O input deve ser no formato:
 # a11, a12, ..., a1n, b1
@@ -70,8 +71,20 @@ def solve_sistema_linear(input: list):
 # 2,1,3
 # 1,-3,-2
 
-arquivo_csv = os.path.join('inputs_para_teste', 'input2.csv')
+print('Escolha um arquivo CSV para resolver o sistema linear:')
 
-arquivo_list = csv_para_list(arquivo_csv)
+arquivo_path = ''
+try:
+    arquivo_path = filedialog.askopenfilename()
+    print('ARQUIVO ESCOLHIDO:', os.path.basename(arquivo_path), '\n')
+except:
+    print('Ocorreu um erro ao escolher o arquivo! Tente novamente...')
+
+try:
+    arquivo_list = csv_para_list(arquivo_path)
+except UnicodeDecodeError:
+    print('O arquivo precisa ser um CSV! Tente novamente...')
+except FileNotFoundError:
+    print('Arquivo escolhido não encontrado! Tente novamente...')
 
 solve_sistema_linear(arquivo_list)
